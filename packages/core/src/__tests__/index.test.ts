8a4169f109c58803494c35a74d1c6880658aa24d
Innei <tukon479@gmail.com>
2022-03-05 17:10:21 +0800

    feat: vitest

--- packages/core/src/__tests__/index.test.ts
+++ packages/core/src/__tests__/index.test.ts
@@ -1,5 +1,11 @@
+import { describe, expect, it, test } from 'vitest'
+import { getTime } from '..'
 describe('test', () => {
   it('should be define', () => {
     expect(test).toBeDefined()
+  })
+
+  it('should getTime', () => {
+    expect(getTime()).toBeDefined()
   })
 })
---
+++ packages/core/vitest.config.ts
@@ -0,0 +1,12 @@
+import { resolve } from 'path'
+import tsconfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from 'vitest/config'
+export default defineConfig({
+  test: {},
+  base: '.',
+  plugins: [
+    tsconfigPaths({
+      projects: [resolve('./tsconfig.json')],
+    }),
+  ],
+})
